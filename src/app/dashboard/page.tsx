1652ff467348efe0b679e265d5ab06eab8676cd5
mbravosarria <mbravosarria@gmail.com>
2023-06-06 12:01:35 -0500

    feat: button component

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Button from "@/components/Button";
+import { FC } from "react";
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <Button>Hello</Button>;
+};
+
+export default page;
