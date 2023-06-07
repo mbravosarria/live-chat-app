ca1e5fb88037b8275d49892fb03b8d8aec1521e8
mbravosarria <mbravosarria@gmail.com>
2023-06-07 08:26:48 -0500

    fix: add alias to storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -12,6 +14,15 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve?.alias,
+        "@": path.resolve(__dirname, "../src/"),
+      };
+    }
+    return config;
   },
 };
 export default config;
