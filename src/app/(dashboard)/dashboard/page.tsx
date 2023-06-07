33caf1d8dfda0809616ee41c2aa41e7df07393f9
mbravosarria <mbravosarria@gmail.com>
2023-06-07 16:27:32 -0500

    refactor: clear default storybook

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+
+const Page = async () => {
+  const session = await getServerSession(authOptions);
+
+  return <pre>{JSON.stringify(session)}</pre>;
+};
+
+export default Page;
